b855aef967ecb11ba7711b9b17c1c15338e76d27
hanseongseo <seeman94@naver.com>
2022-09-30 12:26:29 +0900

    Initial commit

---
+++ src/main/java/mvc1/mvc1practice/Mvc1PracticeApplication.java
@@ -0,0 +1,13 @@
+package mvc1.mvc1practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc1PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mvc1PracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mvc1/mvc1practice/Mvc1PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package mvc1.mvc1practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvc1PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
